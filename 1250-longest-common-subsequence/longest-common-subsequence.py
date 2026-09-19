dcaab5a8a66e6ef2ba08550d3e23edadf7826d20
sumant-here <sumansumant2005@gmail.com>
2026-09-20 00:41:54 +0530

    Time: 415 ms (35.41%) | Memory: 45.2 MB (30.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,18 +1,27 @@
 class Solution:
-    def solve(self,ind1,ind2,text1,text2,dp):
-        if ind1 < 0 or ind2 < 0 :
-            return 0 
-        if dp[ind1][ind2] != -1 :
-            return dp[ind1][ind2]
-        if text1[ind1] == text2[ind2]:
-            dp[ind1][ind2] = 1 + self.solve(ind1-1,ind2-1,text1,text2,dp)
-            return dp[ind1][ind2]
-        dp[ind1][ind2 ] = 0 + max(self.solve(ind1-1,ind2,text1,text2,dp), self.solve(ind1,ind2-1,text1,text2,dp))
-        return dp[ind1][ind2]
+    # def solve(self,ind1,ind2,text1,text2,dp):
+    #     if ind1 < 0 or ind2 < 0 :
+    #         return 0 
+    #     if dp[ind1][ind2] != -1 :
+    #         return dp[ind1][ind2]
+    #     if text1[ind1] == text2[ind2]:
+    #         dp[ind1][ind2] = 1 + self.solve(ind1-1,ind2-1,text1,text2,dp)
+    #         return dp[ind1][ind2]
+    #     dp[ind1][ind2 ] = 0 + max(self.solve(ind1-1,ind2,text1,text2,dp), self.solve(ind1,ind2-1,text1,text2,dp))
+    #     return dp[ind1][ind2]
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-        dp = [[-1 for _ in range(m)]for _ in range(n)]
-        return self.solve(n-1,m-1,text1,text2,dp)
-        
+        dp = [[-1 for _ in range(m+1)]for _ in range(n+1)]
+        for ind2 in range(0,m+1):
+            dp[0][ind2] = 0 
+        for ind1 in range(0,n+1):
+            dp[ind1][0] = 0 
+        for ind1 in  range(1,n+1):
+            for ind2 in range(1,m+1):
+                if text1[ind1-1] == text2[ind2-1]:
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1])
+        return dp[n][m]
